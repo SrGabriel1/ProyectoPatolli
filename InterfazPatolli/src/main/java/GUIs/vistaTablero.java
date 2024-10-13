b6619e72675213efd99dfada919570e9813fbf2f
Gabriel <140923456+SrGabriel1@users.noreply.github.com>
2024-10-13 14:10:17 -0700

    si

--- InterfazPatolli/src/main/java/GUIs/vistaTablero.java
+++ InterfazPatolli/src/main/java/GUIs/vistaTablero.java
@@ -32,11 +32,11 @@ public class vistaTablero extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 625, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 471, Short.MAX_VALUE)
+            .addGap(0, 613, Short.MAX_VALUE)
         );
 
         pack();
