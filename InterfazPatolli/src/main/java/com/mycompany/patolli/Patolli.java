e1ce951168c18ca57327b2e8d2f446fbae11d9eb
Gabriel <140923456+SrGabriel1@users.noreply.github.com>
2024-10-12 19:06:04 -0700

    creacion de las clases

---
+++ InterfazPatolli/src/main/java/com/mycompany/patolli/Patolli.java
@@ -0,0 +1,18 @@
+package com.mycompany.patolli;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Patolli {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
